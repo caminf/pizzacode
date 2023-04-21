cb06b02da4e673c8974f8dcf3f7f1de37804e639
caminf <129898692+caminf@users.noreply.github.com>
2023-04-21 14:51:55 -0400

    Add files via upload

---
+++ App.java
@@ -0,0 +1,19 @@
+package com.pi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+    }
+
+
+    public void bienvenida() {}
+    public void menuSeleccion() {}
+    public void ordenar() {}
+}
