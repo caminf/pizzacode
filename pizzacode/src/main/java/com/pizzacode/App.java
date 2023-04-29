65ccf7411169c924e8f9ffdf817d0dac13a5405e
Carlos <c.pena17@ufromail.cl>
2023-04-29 15:29:01 -0400

    probando cosas aun

--- pizzacode/src/main/java/com/pizzacode/App.java
+++ pizzacode/src/main/java/com/pizzacode/App.java
@@ -9,9 +9,24 @@ public class App
     public static void main( String[] args )
     {
         System.err.println( "Hello World!" );
+        int  [] arr= new int[4];
+    
+        arr = llenarArr(arr);
+        
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
 
-        AEADBadTagException;
+        
+    }
+    public static int[] llenarArr(int [] arr){
+         arr = new int[4];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i]=i;
+        }
+        return arr;
     }
 }
+
 
 
