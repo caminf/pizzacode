ee740f56b4de03cfeb5f03290512a7c493d251a4
caminf <c.infante01@ufromail.cl>
2023-06-27 23:35:23 -0400

    desarrollo tdd y dirtycode

--- pizzacode/src/test/java/com/pizzacode/PizzaTest.java
+++ pizzacode/src/test/java/com/pizzacode/PizzaTest.java
@@ -4,13 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class PizzaTest {
-
-    // mock simula el input de un usaurio por terminal
-
     // la pizza es donde se encuentran los ingredientes y los precios de los
     // ingredientes,
     // aca se arma la pizza
@@ -20,51 +18,37 @@ public class PizzaTest {
     public double costoEsperado = 0;
     public double costoObtenido = 0;
 
-    @BeforeAll
-    public static void setUp() {
+    @BeforeEach
+    public void setUp() {
         ingredientesTest = new ArrayList<>();
         pizzaPrueba = new Pizza("Personalizada");
-        pizza = new Pizza("pizza test");
     }
 
     @ParameterizedTest
     @ValueSource(ints = { 1, 2, 3, 4 })
     public void checkSeleccionarTamanio(int seleccionTamanio) {
-        pizza.seleccionTamanio(seleccionTamanio);
-        Assertions.assertNotNull(pizza.getTamanio());
+        pizzaPrueba.seleccionTamanio(seleccionTamanio);
+        Assertions.assertNotNull(pizzaPrueba.getTamanio());
     }
-
     @ParameterizedTest
     @ValueSource(ints = { 1, 2, 3 })
     public void checkSeleccionarSalsa(int seleccionSalsa) {
-
-        pizza.seleccionarSalsa(seleccionSalsa);
-
-        Assertions.assertNotNull(pizza.getIngredientes());
-
+        pizzaPrueba.seleccionarSalsa(seleccionSalsa);
+        Assertions.assertNotNull(pizzaPrueba.getIngredientes());
     }
 
     @ParameterizedTest
     @ValueSource(ints = { 1 })
-    public void checkCosto(int seleccionIngrediente) {
-        pizza.agregarIngrediente(seleccionIngrediente);
-        pizza.calcularCosto();
-
-        for (int i = 0; i < pizza.getIngredientes().size(); i++) {
-            System.out.println("nombre: " + pizza.getIngredientes().get(i).getNombre());
-            System.out.println("precio: " + pizza.getIngredientes().get(i).getPrecio());
-
-        }
-
-        double costo = pizza.getCosto();
-        /*
+    public void checkCalcularCosto(int seleccionIngrediente) {
+         /*
          * 1.- Tomate______________+$500");
-         * 
-         * La Pizza utilizada por los test se incializa con un costo base de 10
-         * esto para agilizar el testeo de las pizzas
+       
          */
-        double costoEsperado = 500 + 10;
-        Assertions.assertEquals(costoEsperado, costo);
+         pizzaPrueba.agregarIngrediente(seleccionIngrediente);
+         pizzaPrueba.calcularCosto();
+         costoObtenido = pizzaPrueba.getCosto();
+         costoEsperado = 500;
+         Assertions.assertEquals(costoEsperado, costoObtenido);
     }
 
 }
